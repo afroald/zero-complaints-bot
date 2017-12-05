d510d22b47b505e854d5bbc0375e632628a5fbf1
Roald Hacquebord <roald@hacquebordbuckles.nl>
2017-12-05 18:49:57 +0100

    Fix code style  enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (robot) => {
   robot.on('issues.opened', async context => {
-    context.github.issues.addLabels(context.issue({ labels: ['wontfix'] }));
-    context.github.issues.edit(context.issue({ state: 'closed' }));
-  });
+    context.github.issues.addLabels(context.issue({ labels: ['wontfix'] }))
+    context.github.issues.edit(context.issue({ state: 'closed' }))
+  })
 }
